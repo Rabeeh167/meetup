40467b370cc416c36cfb9a61f48bcf7e340cfaca
Rabeeh P <rabp@publicisgroupe.net>
2024-02-15 22:33:24 +0530

    dotnet API walking skeleton

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+        return users;
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser?>> GetUser(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        return user;
+    }
+    //    [HttpGet]
+    //    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    //    {
+    //     var users = _context.Users.ToList();
+    //     return users;
+    //    }
+
+    //    [HttpGet("{id}")]
+    //    public ActionResult<AppUser?> GetUser(int id)
+    //    {
+    //     var user = this._context.Users.Find(id);
+    //     return user;
+    //    }
+}
---
+++ API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public class DataContext: DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<AppUser> Users {get; set;} // Users willbe name of table, id, name will be columns, id willbe primary key by default
+}
+
